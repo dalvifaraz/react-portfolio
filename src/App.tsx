2ba32efe8f9ff2845d70e52d6c101e1d6166753a
Faraz <dalvifaraz@gmail.com>
2023-08-27 01:17:12 +0530

    Header component completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Header from './component/layout/Header';
-import Home from './component/pages/Home';
-import Footer from './component/layout/Footer';
+import React from "react";
+import Header from "./component/layout/Header";
+import Home from "./component/pages/Home";
+import Footer from "./component/layout/Footer";
 
 function App() {
   return (
     <div className="">
-      <Header headerName='faraz' />
+      <Header headerName="Header" />
       <Home />
-      <Footer />
+      <Footer footerName="Footer" />
     </div>
   );
 }
