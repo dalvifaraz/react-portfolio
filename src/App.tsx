036bb99d1f41d3854bb63b4b983b8a32fb397ca4
Faraz <dalvifaraz@gmail.com>
2023-08-29 00:30:33 +0530

    route configuration for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
 import React from "react";
 import { Header, Footer } from "./component/layout";
-import { Home, About, Blog, Contact} from "./component/pages";
+import { Home, About, Blog, Contact, ErrorPage } from "./component/pages";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const routers = createBrowserRouter([
+    {
+      element: <><Header headerName="Header" /><Footer footerName="footer"/></>,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+          errorElement: <ErrorPage />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+          errorElement: <ErrorPage />,
+        },
+        {
+          path: "/blog",
+          element: <Blog />,
+          errorElement: <ErrorPage />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+          errorElement: <ErrorPage />,
+        },
+      ],
+    },
+  ]);
+
   return (
-    <div className="">
-      <Header headerName="Header" />
-      <Home />
-      <About />
-      <Blog />
-      <Contact />
-      <Footer footerName="Footer" />
-    </div>
+    <React.Fragment>
+      <RouterProvider router={routers} />
+    </React.Fragment>
   );
 }
 
--- src/component/pages/index.js
+++ src/component/pages/index.js
@@ -2,9 +2,11 @@ import About from "./About";
 import Blog from "./Blog";
 import Contact from "./Contact";
 import Home from "./Home";
+import ErrorPage from './ErrorPage'
 export {
   About,
   Blog,
   Contact,
   Home,
+  ErrorPage,
 };
